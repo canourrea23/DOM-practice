7c6227512d953bbd2c7c24af12b47907e10431c5
Carolina Urrea <curestrepo@gmail.com>
2022-09-29 21:55:48 -0700

    Changed background colors

---
+++ app.js
@@ -0,0 +1,25 @@
+console.log('UmmpaLummpa')
+
+
+const blue = document.getElementById('blue');
+const red = document.getElementById('red')
+const white = document.getElementById('white')
+const square = document.getElementById('square')
+
+blue.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'blue';
+    console.log('ahh')
+})
+
+red.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'red';
+    console.log('jello')
+})
+
+white.addEventListener('click', () => {
+    document.body.style.backgroundColor = 'white'
+})
+
+square.addEventListener('click', () => {
+    document.c
+})
