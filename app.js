73888054d3f8a9f20a78bb0678986721c4f277a2
Carolina Urrea <curestrepo@gmail.com>
2022-10-05 14:30:54 -0700

    Squares are added

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const blue = document.getElementById('blue');
 const red = document.getElementById('red')
 const white = document.getElementById('white')
 const square = document.getElementById('square')
+const tog = document.getElementById('slider-round')
+
+tog.addEventListener('click', () => {
+    let initialBackgroundColor = document.body.style.backgroundColor;
+    let staticBackgroundColor = new String(document.body.style.backgroundColor).valueOf()
+
+    if (initialBackgroundColor !== "black") {
+        
+        document.body.style.backgroundColor = 'black';
+        console.log('tog')
+    } else {
+        console.log(initialBackgroundColor, 'initial color')
+        console.log(staticBackgroundColor, 'called')
+        document.body.style.backgroundColor = staticBackgroundColor
+    }
+})
+document.body.style.backgroundColor = 'purple'
+console.log(document.body.style.backgroundColor,"haha")
 
 blue.addEventListener('click', () => {
     document.body.style.backgroundColor = 'blue';
@@ -21,5 +39,16 @@ white.addEventListener('click', () => {
 })
 
 square.addEventListener('click', () => {
-    document.c
+    
+    let makeSquare = document.createElement('div')
+    makeSquare.className = 'square'
+    document.body.appendChild(makeSquare)
+    //create ele let new div = document.createelement('div)
+    // let square = document.createElement.apply('div')
+    // square.classList
+    
+    console.log(makeSquare)
 })
+
+//switch is = 0
+// switch is = 1 
